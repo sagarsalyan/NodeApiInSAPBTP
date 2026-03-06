0b808d9ae1ef28d640c110e91fc695b53a86b301
Sagar Salyan <sagarslyn@gmail.com>
2026-03-06 11:04:49 +0000

    Added now route to accept object and print it

--- server.js
+++ server.js
@@ -3,8 +3,27 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World from SAP BTP!");
+  res.send("Running!!");
 });
+
+
+// Middleware to parse JSON body
+app.use(express.json());
+
+
+
+// POST route that accepts an object
+app.post("/print", (req, res) => {
+    const body = req.body;
+
+    console.log("Received object:", body);
+
+    res.json({
+        message: "Object received successfully",
+        received: body
+    });
+});
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
